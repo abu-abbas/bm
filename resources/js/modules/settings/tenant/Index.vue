60f082a05f86af38c772ef75359b4843d093d517
abu-abbas <notificationfor.gitlabs@gmail.com>
2023-10-07 14:06:23 +0700

    fix: design datatable

--- resources/js/modules/settings/tenant/Index.vue
+++ resources/js/modules/settings/tenant/Index.vue
@@ -1,9 +1,61 @@
+<script setup>
+const items = [
+  { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+  { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+  { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+  { age: 38, first_name: 'Jami', last_name: 'Carney' }
+]
+</script>
+
 <template>
   <div class="section-header">
     <h1>Pengaturan Tenant</h1>
   </div>
 
   <div class="section-body">
-    Ini adalah tenant
+    <h2 class="section-title fs-lg">Daftar Tenant</h2>
+    <p class="section-lead">
+      Daftar tenant yang akan ditampilkan pada halaman depan
+    </p>
+
+    <div class="row">
+      <div class="col-12">
+        <div class="card border-gray-light">
+          <div class="card-body">
+            <div class="action-wrapper d-flex mb-3 flex-wrap align-items-center justify-content-between">
+              <div class="left-section">
+                <button class="btn btn-icon icon-left btn-primary">
+                  <FontAwesomeIcon :icon="['fas', 'plus']" />
+                  Tambah
+                </button>
+              </div>
+              <div class="right-section mt-2 mt-md-0">
+                <div class="input-group">
+                  <div class="input-group-prepend">
+                    <div class="input-group-text">
+                      <FontAwesomeIcon :icon="['fas', 'search']" />
+                    </div>
+                  </div>
+                  <input
+                    type="text"
+                    class="form-control"
+                    placeholder="Pencarian"
+                  >
+                </div>
+              </div>
+            </div>
+
+            <b-table
+              table-class="custom-bordered dataTable"
+              :items="items"
+              hover
+              striped
+              responsive
+            >
+            </b-table>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
