e810442088a1e4beacbfb6299a36694b46fc3224
ianfajar10 <ian.fajar10@gmail.com>
2023-12-10 09:43:03 +0700

    recompile

---
+++ public/build/assets/compiled/js/modules/chunk/product/main-57b4539c.js
@@ -0,0 +1,1 @@
+import{K as e,L as r,x as o,F as a,m as c}from"../../../vendor/vue-utils-cea615f1.js";import{_ as s}from"../../resource/_plugin-vue_export-helper-c27b6911.js";const _={};function m(i,l){const t=e("section-header"),n=e("router-view");return c(),r(a,null,[o(t,{title:"Master Barang"}),o(n)],64)}const d=s(_,[["render",m]]);export{d as default};
---
+++ public/build/assets/compiled/js/modules/chunk/user/main-4f74bf7c.js
@@ -0,0 +1,1 @@
+import{K as e,L as n,x as o,F as s,m as c}from"../../../vendor/vue-utils-cea615f1.js";import{_ as a}from"../../resource/_plugin-vue_export-helper-c27b6911.js";const _={};function m(i,l){const t=e("section-header"),r=e("router-view");return c(),n(s,null,[o(t,{title:"Master Users"}),o(r)],64)}const d=a(_,[["render",m]]);export{d as default};
