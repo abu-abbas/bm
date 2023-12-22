f7509c5f77c0282cf8e0a13507978148020e019c
ianfajar10 <ian.fajar10@gmail.com>
2023-12-23 00:29:46 +0700

    recompile

--- public/build/assets/compiled/js/modules/chunk/tenant/main-8558a543.js
+++
@@ -1,1 +0,0 @@
-import{K as e,L as r,x as t,F as c,m as a}from"../../../vendor/vue-utils-cea615f1.js";import{_ as s}from"../../resource/_plugin-vue_export-helper-c27b6911.js";const _={};function m(i,l){const o=e("section-header"),n=e("router-view");return a(),r(c,null,[t(o,{title:"Master Tenant"}),t(n)],64)}const d=s(_,[["render",m]]);export{d as default};
